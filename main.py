e9a7e4ec8dc65602b9090a69bedbeace6e29398c
anddhe170353 <anhddhe170353@fpt.edu.vn>
2023-06-30 16:39:21 +0700

    body - fields

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from builtins import min
 from enum import Enum
+from turtle import title
+
 from fastapi import Body, FastAPI, Query, Path
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -145,41 +147,54 @@ app = FastAPI()
 
 
 #Part 7 -> Body - Multiple Parameters
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#
+#
+# class User(BaseModel):
+#     username: str
+#     full_name: str | None = None
+#
+#
+# class Importance(BaseModel):
+#     importance: int
+#
+# @app.put("/items/{item_id}")
+# async def update_item(
+#         *,
+#         item_id: int = Path(..., title="The ID of the item to get", ge=0, le=150),
+#         q: str | None = None,
+#         item: Item = Body(..., embed=True),
+#         # user: User,
+#         # importance: int = Body(...)
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     if item:
+#         results.update({"item": item})
+#     # if user:
+#     #     results.update({"user": user})
+#     # if importance:
+#     #     results.update({"importance": importance})
+#     return results
+
+##Part 8 -> Body -> Fields
+
 class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
+    description: str | None = Field(None, title="the description of the item", max_length=300)
+    price: float = Field(..., gt=0, description="The price must be greater than zero")
     tax: float | None = None
 
 
-class User(BaseModel):
-    username: str
-    full_name: str | None = None
-
-
-class Importance(BaseModel):
-    importance: int
-
 @app.put("/items/{item_id}")
-async def update_item(
-        *,
-        item_id: int = Path(..., title="The ID of the item to get", ge=0, le=150),
-        q: str | None = None,
-        item: Item | None = None,
-        user: User,
-        importance: int = Body(...)
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if item:
-        results.update({"item": item})
-    if user:
-        results.update({"user": user})
-    if importance:
-        results.update({"importance": importance})
+async def update_item(item_id: int, item: Item = Body(..., embed=True)):
+    results = {"item_id": item_id, "item": item}
     return results
-
 
 
 
